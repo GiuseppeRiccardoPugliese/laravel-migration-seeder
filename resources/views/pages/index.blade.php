bd2f54c9be1bde521c487f83deb6c59aa93960a7
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-13 17:20:56 +0100

    Visualizzare a schermo gli elementi contenuti nel DB

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.main-layout')
 @section('head')
-    <title>Home</title>
+    <title>laravel-migration-seeder</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>Treni: {{ count($trains) }}</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                {{-- <th scope="col"></th> --}}
+                <th scope="col">Company</th>
+                <th scope="col">Departure Station</th>
+                <th scope="col">Arrival Station</th>
+                <th scope="col">Departure Time</th>
+                <th scope="col">Arrival Time</th>
+                <th scope="col">Code Of Train</th>
+                <th scope="col">Carriages Number</th>
+                <th scope="col">In Time</th>
+                <th scope="col">Deleted</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>{{ $train->company }}</td>
+                    <td>{{ $train->departure_station }}</td>
+                    <td>{{ $train->arrival_station }}</td>
+                    <td>{{ $train->departure_time }}</td>
+                    <td>{{ $train->arrival_time }}</td>
+                    <td>{{ $train->train_code }}</td>
+                    <td>{{ $train->carriages_number }}</td>
+                    <td>{{ $train->in_time }}</td>
+                    <td>{{ $train->deleted }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
